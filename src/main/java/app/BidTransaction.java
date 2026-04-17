4ef2fe976a4750a299ed131d7bd1fd601b2fee81
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-04-17 08:24:04 +0700

    BidTransaction.java

--- src/main/java/app/BidTransaction.java
+++ src/main/java/app/BidTransaction.java
@@ -1,5 +1,34 @@
 package app;
+import java.time.LocalDateTime;
 
 public class BidTransaction {
+    private Bidder bidder;
+    private double amount;
+    private LocalDateTime time;
+    public BidTransaction(Bidder bidder, double amount, LocalDateTime time) {
+        this.bidder = bidder;
+        this.amount = amount;
+        this.time = time;
+    }
+    public Bidder getBidder() {
+        return bidder;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public LocalDateTime getTime() {
+        return time;
+    }
+    public void setBidder(Bidder bidder) {
+        this.bidder = bidder;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
     
+
+
 }
