7c9233fa98a4fc4e8a7a5bb383ea1aa67cfd730b
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-04-22 11:39:46 +0700

    fgh

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,27 @@
 package app;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+        Label label = new Label("Chào mừng đến với Hệ thống Đấu giá!");
+        StackPane root = new StackPane(label);
+        
+        Scene scene = new Scene(root, 800, 600);
+        
+        primaryStage.setTitle("Ứng dụng Đấu Giá Trực Tuyến - Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("ok");
+        // Khởi chạy giao diện JavaFX
+        launch(args);
     }
 }
--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -1,5 +1,32 @@
 package app;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-    
+    private static final int PORT = 8080;
+    private ExecutorService clientPool = Executors.newFixedThreadPool(50);
+
+    public void startServer() {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("=== 🚀 SERVER ĐẤU GIÁ ĐANG CHẠY Ở PORT " + PORT + " ===");
+
+            while (true) {
+
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("🟢 Có Client mới kết nối: " + clientSocket.getInetAddress());
+
+            }
+        } catch (IOException e) {
+            System.err.println("❌ Lỗi khởi chạy Server: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Khởi chạy Server
+        Server server = new Server();
+        server.startServer();
+    }
 }
