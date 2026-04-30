0db831945c017d968a38dbe295af24d3d753133a
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-04-30 23:53:19 +0700

    12

--- src/main/java/app/model/Message.java
+++ src/main/java/app/model/Message.java
@@ -2,4 +2,15 @@ package app.model;
 public class Message {
     private String type;
     private String payload;
+    public Message(String type, String payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getPayload() {
+        return payload;
+    }
+    
 }
