3b90342cdf17a938d3c312ee4bba777736f6694d
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-05-01 00:01:03 +0700

    login

--- src/main/java/app/controller/LoginController.java
+++ src/main/java/app/controller/LoginController.java
@@ -1,5 +1,10 @@
 package app.controller;
 
+import com.google.gson.Gson;
+
+import app.model.Bidder;
+import app.model.Message;
+import app.network.ClientConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,6 +37,23 @@ public class LoginController {
         }
 
         System.out.println("Đang đăng nhập tài khoản: " + username);
+
+    
+        try {
+      
+            ClientConnection.getInstance().connect("localhost", 8080);
+            
+     
+            Bidder loginUser = new Bidder("temp_id", username, password);
+            String userPayload = new Gson().toJson(loginUser); 
+            
+       
+            Message loginMsg = new Message("LOGIN", userPayload);
+            ClientConnection.getInstance().sendMessage(loginMsg);
+        } catch (Exception e) {
+            System.out.println("Lỗi gửi dữ liệu lên Server: " + e.getMessage());
+        }
+   
 
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
