a059e169d255dd47a10df7ff2cf0c29267aa4ed0
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-05-12 10:42:01 +0700

    thay đổi lưu file JSON bằng kết nối socket gửi  lên thẳng server

--- src/main/java/app/controller/SignupController.java
+++ src/main/java/app/controller/SignupController.java
@@ -1,10 +1,13 @@
 package app.controller;
 
-import java.util.UUID;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-import app.database.AppDatabase;
-import app.model.Account;
-import app.model.AccountRole;
+import com.google.gson.Gson;
+
+import app.model.Message;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,21 +35,17 @@ public class SignupController {
     @FXML
     private Label errorLabel;
 
+
+    private final int SERVER_PORT = 8080;
+    private final String SERVER_IP = "localhost";
+
     @FXML
     public void handleRegisterAction() {
         String fullName = fullNameField.getText().trim();
         String email = emailField.getText().trim();
         String password = passwordField.getText();
         String confirmPassword = confirmPasswordField.getText();
-        try {
-            app.model.Seller newSeller = new app.model.Seller("ID_TEMP", email, password);
 
-            app.SecurityUtils.saveAccount(newSeller, "account_data.json");
-
-            System.out.println("Đăng ký thành công! Đã lưu và mã hóa vào account_data.json");
-        } catch (Exception e) {
-            System.out.println("Lỗi lưu file: " + e.getMessage());
-        }
 
         if (fullName.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             showMessage("Vui lòng nhập đầy đủ thông tin!", "red");
@@ -58,29 +57,34 @@ public class SignupController {
             return;
         }
 
-        AppDatabase database = AppDatabase.getInstance();
-        if (database.usernameExists(email)) {
-            showMessage("Tài khoản đã tồn tại", "red");
-            return;
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            Message msg = new Message("SIGNUP", email + ":" + password);
+            Gson gson = new Gson();
+            out.println(gson.toJson(msg));
+
+         
+            String response = in.readLine();
+
+            if ("SUCCESS".equals(response)) {
+                showMessage("Đăng ký thành công!", "green");
+
+                
+                PauseTransition pause = new PauseTransition(Duration.seconds(1.2));
+                pause.setOnFinished(e -> goBackToLogin());
+                pause.play();
+            } else if ("FAIL_EXISTS".equals(response)) {
+                showMessage("Tài khoản (Email) đã tồn tại!", "red");
+            } else {
+                showMessage("Không thể tạo tài khoản!", "red");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            showMessage("Không thể kết nối đến Server!", "red");
         }
-
-        Account account = new Account(
-                "U_" + UUID.randomUUID(),
-                email,
-                password,
-                AccountRole.GUEST
-        );
-
-        if (!database.addAccount(account)) {
-            showMessage("Không thể tạo tài khoản!", "red");
-            return;
-        }
-
-        showMessage("Đăng ký thành công!", "green");
-
-        PauseTransition pause = new PauseTransition(Duration.seconds(1.2));
-        pause.setOnFinished(e -> goBackToLogin());
-        pause.play();
     }
 
     @FXML
