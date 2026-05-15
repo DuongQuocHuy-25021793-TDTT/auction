5903967187deed901f812678accb0405089ad55d
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-05-15 09:17:25 +0700

    thêm hàm ở message

--- src/main/java/app/model/Message.java
+++ src/main/java/app/model/Message.java
@@ -1,16 +1,33 @@
 package app.model;
+
 public class Message {
     private String action;
     private String data;
+
     public Message(String action, String data) {
         this.action = action;
         this.data = data;
     }
+
     public String getAction() {
         return action;
     }
+
     public String getData() {
         return data;
     }
+
+
+    public String getCommand() {
+        return action; 
+    }
+
     
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
--- src/main/java/app/network/Server.java
+++ src/main/java/app/network/Server.java
@@ -112,9 +112,74 @@ class ClientHandler implements Runnable {
             out = new PrintWriter(socket.getOutputStream(), true);
             String jsonMessage;
 
+            
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            
+            
+            com.google.gson.Gson gsonDate = new com.google.gson.GsonBuilder()
+                .registerTypeAdapter(java.time.LocalDateTime.class, new com.google.gson.TypeAdapter<java.time.LocalDateTime>() {
+                    @Override
+                    public void write(com.google.gson.stream.JsonWriter out, java.time.LocalDateTime value) throws java.io.IOException {
+                        out.value(value != null ? value.toString() : null);
+                    }
+                    @Override
+                    public java.time.LocalDateTime read(com.google.gson.stream.JsonReader in) throws java.io.IOException {
+                        return java.time.LocalDateTime.parse(in.nextString());
+                    }
+                }).create();
+
             while ((jsonMessage = in.readLine()) != null) {
-                System.out.println("SERVER NHẬN ĐƯỢC TỪ CLIENT: " + jsonMessage);
+                System.out.println("SERVER NHẬN ĐƯỢC: " + jsonMessage);
+
+                try {
+                    
+                    app.model.Message msg = gson.fromJson(jsonMessage, app.model.Message.class);
+                    if (msg == null) continue;
+
                 
+                    switch (msg.getCommand()) {
+                        
+                        case "BID":
+                        
+                            app.model.BidTransaction bid = gsonDate.fromJson(msg.getData(), app.model.BidTransaction.class);
+                            app.model.Auction auction = app.database.AppDatabase.getInstance().findAuctionById(bid.getAuctionId());
+                            
+                            if (auction != null) {
+                         
+                                String validation = auction.validateBid(bid);
+                                
+                                if (validation == null) {
+                                    auction.placeBid(bid); 
+                                    sendMessage("SUCCESS"); 
+                                    
+                                 
+                                    Server.broadcast("UPDATE_AUCTION|" + auction.getId()); 
+                                } else {
+                             
+                                    sendMessage("FAIL_" + validation); 
+                                }
+                            } else {
+                                sendMessage("ERROR");
+                            }
+                            break;
+
+                        case "GET_HISTORY":
+                            String reqAuctionId = msg.getData();
+                         
+                            List<String> historyStrings = app.database.AppDatabase.getInstance().getBidHistory(reqAuctionId);
+                     
+                            sendMessage(gson.toJson(historyStrings));
+                            break;
+
+                        default:
+                            System.out.println("Không nhận diện được lệnh: " + msg.getCommand());
+                            break;
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Lỗi xử lý JSON từ Client: " + e.getMessage());
+                    sendMessage("ERROR");
+                }
             }
         } catch (IOException e) {
             System.out.println(" [-] Một Client đã ngắt kết nối.");
