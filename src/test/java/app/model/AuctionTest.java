b6b795920decf1abb092a2ea101ac6e671844153
DuongQuocHuy-25021793-TDTT <duongquochuya2k18dpt@gmail.com>
2026-05-23 14:54:48 +0700

    tạo file kiểm thử

--- src/test/java/app/model/AuctionTest.java
+++ src/test/java/app/model/AuctionTest.java
@@ -1,10 +1,12 @@
 package app.model;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.LocalDateTime;
 
 public class AuctionTest {
 
@@ -13,32 +15,29 @@ public class AuctionTest {
 
     @BeforeEach
     public void setUp() {
-        // Hàm này tự động chạy trước mỗi bài test để tạo ra một phiên đấu giá mẫu
+        
         item = new Art("I_TEST", "Tranh Hoa Hướng Dương", "Tranh sơn dầu", 1000.0, "Van Gogh", 1888);
         auction = new Auction("A_TEST", item, 1000.0);
     }
 
     @Test
     public void testValidateBid_Success() {
-        // Kịch bản 1: Người dùng đặt giá hợp lệ (1500 > 1000)
+        
         BidTransaction validBid = new BidTransaction("BID_01", "A_TEST", "UserA", 1500.0, LocalDateTime.now());
         
-        // Trả về null nghĩa là không có lỗi (Hợp lệ)
         assertNull(auction.validateBid(validBid), "Giá 1500 phải được chấp nhận vì lớn hơn 1000");
     }
 
     @Test
     public void testValidateBid_TooLow() {
-        // Kịch bản 2: Người dùng đặt giá thấp hơn giá hiện tại (900 < 1000)
+   
         BidTransaction lowBid = new BidTransaction("BID_02", "A_TEST", "UserB", 900.0, LocalDateTime.now());
-        
-        // Phải trả về lỗi "TOO_LOW"
+     
         assertEquals("TOO_LOW", auction.validateBid(lowBid), "Phải báo lỗi TOO_LOW khi đặt giá thấp hơn giá khởi điểm");
     }
 
     @Test
     public void testValidateBid_InvalidAmount() {
-        // Kịch bản 3: Người dùng cố tình hack hệ thống, đặt giá âm (-500)
         BidTransaction negativeBid = new BidTransaction("BID_03", "A_TEST", "UserC", -500.0, LocalDateTime.now());
       
         assertEquals("INVALID_AMOUNT", auction.validateBid(negativeBid), "Phải báo lỗi INVALID_AMOUNT khi giá trị âm");
