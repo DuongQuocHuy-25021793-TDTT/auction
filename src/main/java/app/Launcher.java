f61ae9100da11b0ec961ffdc36775fa4fab17d5e
nguyengiahung261207 <nguyengiahieu261207@gmail.com>
2026-05-31 15:46:07 +0700

    gọi giao diện lên mà không bị lỗi   hệ thống

---
+++ src/main/java/app/Launcher.java
@@ -0,0 +1,10 @@
+package app;
+
+/**
+ * Lớp khởi chạy trung gian để khắc phục lỗi "JavaFX runtime components are missing".
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
